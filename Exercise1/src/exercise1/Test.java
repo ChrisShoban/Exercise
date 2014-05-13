b601020ab6bce3e0660b3c598f54e431daca6bf1
sampriya <sampriya@localhost>
2014-05-12 23:07:51 -0700

    exercise 1

--- Exercise1/src/exercise1/Test.java
+++ Exercise1/src/exercise1/Test.java
@@ -3,10 +3,6 @@ package exercise1;
 public class Test {
 
 	public static void main() {
-		// TODO: Please test with following values.
-		
-		/**
-		 * Please use the 3 graphs in Google Docs to test your code
-		 * */
+		// TODO: Please use the 3 graphs in Google Docs to test your code
 	}
 }
